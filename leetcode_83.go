ff4e2f7bc55e2dbbd028c15be2cdc8dcffdcb9e4
Igor Tin <itin@dar.kz>
2019-05-04 15:49:35 +0600

    wip

--- leetcode_83.go
+++ leetcode_83.go
@@ -1,10 +1,3 @@
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
 func deleteDuplicates(head *ListNode) *ListNode {
     if head == nil {
         return nil
