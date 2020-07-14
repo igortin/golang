d8138ca0b02da84c72b094f40fef6946168bf44d
itin <tin.igrik@gmail.com>
2020-07-14 14:07:03 +0600

    remove empty lines

--- leetcode_67.go
+++ leetcode_67.go
@@ -3,6 +3,7 @@ func maxSubArray(nums []int) int {
 	var MaxInt  = int(^uint(0) >> 1)
 	var MinInt  = -MaxInt - 1
 	var maxSuma = MinInt
+	
 	for _, v := range nums {
 		sum = sum + v
 		if sum < 0 {
@@ -10,16 +11,16 @@ func maxSubArray(nums []int) int {
 		}
 		maxSuma = max(sum,maxSuma)
 	}
+	
     if maxSuma == 0 {
         var no = nums[0] 
         for _, v := range nums{
             no=max(no,v)
         }
-        return no
-    }
-	return maxSuma
+    return no
+   }
+return maxSuma
 }
-
 
 func max(x, y int) int {
 	if x > y {
